f712f3f7263494643b541085ca38cc385c70df4f
skipzero <skip@zerosquadron.com>
2015-04-23 13:32:14 -0700

    more attr and colours

--- README.md
+++ README.md
@@ -3,6 +3,8 @@ a little script to change the CSS values of all files in a dir.
 
 
 # To run:
-1. navigate to node folder 
+1. navigate to xformCss folder 
 
-2. type node index.js
+2. type node index.js <args>
+
+  if only one <arg> is specified, it will apply to 
--- index.js
+++ index.js
@@ -3,22 +3,32 @@
 var fs			= require('fs')
 	, _ 		= require('underscore')
 	, chalk 	= require('chalk')
-	, path		= __dirname + '/../../Desktop/NGL_ELT-GREX1/OPS/skip/'
+	, path		= __dirname + '/../../Desktop/NGL_ELT-GREX1/OPS/styles/'
 	, red 		= chalk.red
 	, blue 		= chalk.blue
 	, grn 		= chalk.green
-	, ylw 		= chalk.yellow
-	, left 		= /\bleft?\b:\s*[0-9]*.*/gmi
-	, right 	= /\bright?\b:\s*[0-9]*.*/gmi
-	, top 		= /\btop?\b:\s*[0-9]*.*/gmi
-	, bottom 	= /\bbottom\b:\s*[0-9]*.*/gmi
-	, height 	= /\bheight\b:\s*[0-9]*.*/gmi
-	, float 	= /\bfloat\b:\s*[0-9]*.*/gmi
-	, dec 		= /(?:\d*\.)?\d+/gmi;
+	, ylw 		= chalk.yellow;
+
+	//  TODO: Check transforms are all in %, font-size, line-height, letter-spacing, word-spacing, margin, padding
+
+				console.log(chalk.magenta(process.argv))
 
 readCssFiles(process.argv[2]);
 
 function readCssFiles(times) {
+
+	//  Object containing all properties we're concerned with
+	var css = {
+		'top' 		: /\btop?\b:\s*[0-9]*.*/gmi,
+		'right' 	: /\bright?\b:\s*[0-9]*.*/gmi,
+		'bottom' 	: /\bbottom\b:\s*[0-9]*.*/gmi,
+		'left' 		: /\bleft?\b:\s*[0-9]*.*/gmi,
+		'height' 	: /\bheight\b:\s*[0-9]*.*/gmi,
+		'width' 	: /\bwidth\b:\s*[0-9]*.*/gmi,
+		'dec' 		: /(?:\d*\.)?\d+/gmi,
+		'per' 		: /%/gm
+	}
+
 	fs.readdir (path, function(err, files) {
 		if (err) {
 			console.log('Error reading dir:', err);
@@ -31,34 +41,50 @@ function readCssFiles(times) {
 					console.log('Error reading file:', err);
 				}
 
-				if ( data.match(top) || data.match(right) || data.match(bottom) ||data.match(left) ) {
+				if ( data.match(css.top) || data.match(css.right) || data.match(css.bottom) ||data.match(css.left) ) {
 
+					var tmp = data;
 					//console.log(chalk.bgBlue(data))
 
-					if (data.match(top)) {
-						var showTop 	= data.match(top);
+					if (tmp.match(css.top)) {
+					tmp = tmp.split('\n')
+					console.log(tmp)
+						var showTop 	= data.match(css.top);
+						if (data.match(css.per)) {
+							console.log(chalk.bgGreen('BANGHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH'))
+						}
 						console.log(grn('^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^', showTop));
 
 					}
 
-					if (data.match(right)) {
-						var showRt = data.match(right);
+					if (data.match(css.right)) {
+						var showRt = data.match(css.right);
+						if (data.match(css.per)) {
+							console.log(chalk.bgRed('BANGHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH'))
+						}
 						console.log(red('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>', showRt));
 
 					} 
 
-					if (data.match(bottom)) {
-						var showBtm = data.match(bottom) 
+					if (data.match(css.bottom)) {
+						var showBtm = data.match(css.bottom);
+						if (data.match(css.per)) {
+							console.log(chalk.bgYellow('BANGHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH'))
+						}
 						console.log(ylw('++++++++++++++++++++++++++++++++++++++++++++++++++', showBtm));
 
 					} 
 
-					if (data.match(left)) {
-						var showLeft 	= data.match(left);
+					if (data.match(css.left)) {
+						var showLeft 	= data.match(css.left);
 
-						console.log(blue('<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<', data.match(left)))
+						if (data.match(css.per)) {
+							console.log(chalk.bgBlue('BANGHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH'))
+						}
+
+						console.log(blue('<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<', data.match(css.left)))
 						_.each(showLeft, function(showLeft) {
-							var ltNum = showLeft.match(dec);
+							var ltNum = showLeft.match(css.dec);
 							//ltNum = Number.parseFloat((ltNum * times).toFixed(2));
 							showLeft = 'left:' + ltNum + 'px;';
 							//console.log(chalk.bgCyan(ltNum, showLeft))
