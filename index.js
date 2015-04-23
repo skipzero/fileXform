db0d946a9f3f7ad6d4d115949385fe12f5edf50d
skipzero <skip@zerosquadron.com>
2015-04-23 10:59:04 -0700

    top, right, bottom and left properties added

--- index.js
+++ index.js
@@ -4,21 +4,21 @@ var fs			= require('fs')
 	, _ 		= require('underscore')
 	, chalk 	= require('chalk')
 	, path		= __dirname + '/../../Desktop/NGL_ELT-GREX1/OPS/skip/'
+	, red 		= chalk.red
+	, blue 		= chalk.blue
+	, grn 		= chalk.green
+	, ylw 		= chalk.yellow
 	, left 		= /\bleft?\b:\s*[0-9]*.*/gmi
-	, right 	= /\bright?\b:/gmi
-	, top 		= /\btop?\b:/gmi
-	, bottom 	= /\bbottom\b:/gmi
-	, height 	= /\bheight\b:/gmi
-	, float 	= /\bfloat\b:/gmi
-	, dec 		= /(?:\d*\.)?\d+/g;
+	, right 	= /\bright?\b:\s*[0-9]*.*/gmi
+	, top 		= /\btop?\b:\s*[0-9]*.*/gmi
+	, bottom 	= /\bbottom\b:\s*[0-9]*.*/gmi
+	, height 	= /\bheight\b:\s*[0-9]*.*/gmi
+	, float 	= /\bfloat\b:\s*[0-9]*.*/gmi
+	, dec 		= /(?:\d*\.)?\d+/gmi;
 
 readCssFiles(process.argv[2]);
 
 function readCssFiles(times) {
-
-
-
-console.log(process.argv)
 	fs.readdir (path, function(err, files) {
 		if (err) {
 			console.log('Error reading dir:', err);
@@ -31,19 +31,40 @@ console.log(process.argv)
 					console.log('Error reading file:', err);
 				}
 
-				if (data.match(left)) {
-					var showLeft 	= data.match(left);
+				if ( data.match(top) || data.match(right) || data.match(bottom) ||data.match(left) ) {
 
-					console.log(chalk.green('===>================================================================================================================='))
-					console.log(chalk.blue.bold(data.match(left)))
-					// _.each(showLeft, function(showLeft) {
-					// 	var ltNum = showLeft.match(dec);
-					// 	ltNum = Number.parseFloat((ltNum * times).toFixed(2));
-					// 	showLeft = 'left:' + ltNum + 'px;';
-					// 	console.log(showLeft)
-					// 	//fs.writeFileSync(path + file, data);
-					// })
-					// console.log(showLeft)
+					//console.log(chalk.bgBlue(data))
+
+					if (data.match(top)) {
+						var showTop 	= data.match(top);
+						console.log(grn('^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^', showTop));
+
+					}
+
+					if (data.match(right)) {
+						var showRt = data.match(right);
+						console.log(red('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>', showRt));
+
+					} 
+
+					if (data.match(bottom)) {
+						var showBtm = data.match(bottom) 
+						console.log(ylw('++++++++++++++++++++++++++++++++++++++++++++++++++', showBtm));
+
+					} 
+
+					if (data.match(left)) {
+						var showLeft 	= data.match(left);
+
+						console.log(blue('<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<', data.match(left)))
+						_.each(showLeft, function(showLeft) {
+							var ltNum = showLeft.match(dec);
+							//ltNum = Number.parseFloat((ltNum * times).toFixed(2));
+							showLeft = 'left:' + ltNum + 'px;';
+							//console.log(chalk.bgCyan(ltNum, showLeft))
+							//fs.writeFileSync(path + file, data);
+						})
+					} 
 				}
 			})
 		}
