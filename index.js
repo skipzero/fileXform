d62c2e2b4b200348df6ac668d7cf62d4b53efb96
skipzero <skip@zerosquadron.com>
2015-04-22 18:16:29 -0700

    added regex and can read value

--- index.js
+++ index.js
@@ -1,33 +1,48 @@
 /* global require, _ */
 'use strict';
 
-var fs		= require('fs')
-	, _ 	= require('underscore')
-	, path	= __dirname + '/../../Desktop/NGL_ELT-GREX1_v3/OPS/styles/';
+var fs			= require('fs')
+	, _ 		= require('underscore')
+	, path		= __dirname + '/../../Desktop/NGL_ELT-GREX1/OPS/styles/'
+	, left 		= /\bleft?\b:\s*[0-9]*.*/gmi
+	, right 	= /\bright?\b:/gmi
+	, top 		= /\btop?\b:/gmi
+	, bottom 	= /\bbottom\b:/gmi
+	, height 	= /\bheight\b:/gmi
+	, float 	= /\bfloat\b:/gmi
+	, dec 		= /(?:\d*\.)?\d+/g;
 
-function readFiles(path) {
+readCssFiles(process.argv[2]);
 
+function readCssFiles(times) {
+console.log(process.argv)
 	fs.readdir (path, function(err, files) {
 		if (err) {
 			console.log('Error reading dir:', err);
 		} else {
-			readFile(files)
+			_.each(files, function(file) {
+				console.log(path + file);
+				try {
+					var data = fs.readFileSync(path + file, 'utf8');
+				} catch (err) {
+					console.log('Error reading file:', err);
+				}
+
+				if (data.match(left)) {
+					var showLeft 	= data.match(left);
+					console.log('===>=================================================================================================================')
+					console.log(data.match(left))
+					// _.each(showLeft, function(showLeft) {
+					// 	var ltNum = showLeft.match(dec);
+					// 	ltNum = Number.parseFloat((ltNum * times).toFixed(2));
+					// 	showLeft = 'left:' + ltNum + 'px;';
+					// 	console.log(showLeft)
+					// 	//fs.writeFileSync(path + file, data);
+					// })
+//					console.log(showLeft)
+				}
+			})
 		}
 	});
 }
 
-function readFile(files) {
-	_.each(files, function(file) {
-		console.log(path + file);
-		fs.readFile(path + file, 'utf8', function(err, data) {
-			if (err) {
-				console.log('Error reading file:', err)
-			} else {
-				console.log(file, data)
-			}
-		})
-	})
-}
-
-
-readFiles(path)
